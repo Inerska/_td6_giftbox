683cd80833258063f8ce4175e6656dc3c638c6b4
Alexis C. Gridel <contact@agdl.dev>
2023-06-11 17:04:00 +0200

    feat(service/prestation): create categorie

--- gift.appli/src/services/PrestationsService.php
+++ gift.appli/src/services/PrestationsService.php
@@ -2,29 +2,59 @@
 
 namespace gift\app\services;
 
+use Exception;
 use gift\app\models\Categorie;
 use gift\app\models\Prestation;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class PrestationsService
 {
-
     public static function getPrestationById(string $id): array
     {
         try {
-            return Prestation::findOrFail($id)->toArray();
+            return Prestation::findOrFail($id)
+                ->toArray();
         } catch (ModelNotFoundException $e) {
-            throw new PrestationNotFoundException("Prestation inconnue: ". $id);
+            throw new PrestationNotFoundException("Prestation inconnue: " . $id);
         }
     }
 
     public static function getPrestationsByCategorie(int $categ_id): array
     {
         try {
-            return Categorie::findOrFail($categ_id)->prestations()->get()->toArray();
+            return Categorie::findOrFail($categ_id)
+                ->prestations()
+                ->get()
+                ->toArray();
+
         } catch (ModelNotFoundException $e) {
-            throw new PrestationNotFoundException("Catégorie inconnue ou sans prestation: ". $categ_id);
+            throw new PrestationNotFoundException("Catégorie inconnue ou sans prestation: " . $categ_id);
         }
+    }
+
+    /**
+     * @throws Exception if data is invalid
+     */
+    public static function createCategory(array $data): array
+    {
+        if (!isset($data['libelle']) || !isset($data['description'])) {
+            throw new Exception('Invalid data: libelle or description not provided');
+        }
+
+        $libelle = htmlspecialchars($data['libelle']);
+        $description = htmlspecialchars($data['description']);
+
+        if ($libelle !== $data['libelle']
+            || $description !== $data['description']) {
+            throw new Exception('Invalid data: libelle or description contains invalid characters');
+        }
+
+        $category = Categorie::create([
+            'libelle' => $libelle,
+            'description' => $description
+        ]);
+
+        return $category->toArray();
     }
 
 }
