ce09a1a4a647a5c644a4d8be6c19bb88ed292e4b
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-06-12 15:10:22 +0200

    fix(template/prestation): per id

---
+++ gift.appli/src/infrastructure/exceptions/PrestationIdNotProvidenException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace gift\app\infrastructure\exceptions;
+
+use PHPUnit\Framework\Exception;
+
+final class PrestationIdNotProvidenException extends Exception
+{
+
+}
