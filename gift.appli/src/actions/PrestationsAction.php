6da3ac450674a572548e9ff3479ec443dfae1450
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-06-12 14:26:38 +0200

    fix(prestation): display prestations by category id

--- gift.appli/src/actions/PrestationsAction.php
+++ gift.appli/src/actions/PrestationsAction.php
@@ -2,23 +2,24 @@
 
 namespace gift\app\actions;
 
+use Exception;
 use gift\app\services\PrestationsService;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
 
-class PrestationsAction extends Action
+final class PrestationsAction extends Action
 {
 
     public function __invoke(Request $request, Response $response, $args): Response
     {
-        if(!isset($request->getQueryParams()['id'])){
-            throw new \Exception("L'id de la categorie n'est pas renseigné");
+        if (!isset($request->getQueryParams()['id'])) {
+            throw new Exception("L'id de la categorie n'est pas renseigné");
         }
 
         return Twig::fromRequest($request)->render($response, 'prestations.twig',
-            ['prestations' => PrestationsService::getPrestationsByCategorie(
-                    $request->getQueryParams()['id']
+            ['prestations' => PrestationsService::getPrestationsByCategorieId(
+                $request->getQueryParams()['id']
             )]
         );
     }
