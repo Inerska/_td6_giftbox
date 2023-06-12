d1e4ae710f0c37cf2194b1405fcc380ff35b4b6e
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-06-12 14:29:28 +0200

    refactor(prestation): more specific exception for non-providen category id

--- gift.appli/src/actions/PrestationsAction.php
+++ gift.appli/src/actions/PrestationsAction.php
@@ -2,7 +2,7 @@
 
 namespace gift\app\actions;
 
-use Exception;
+use CategoryIdNotProvidenException;
 use gift\app\services\PrestationsService;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -14,7 +14,7 @@ final class PrestationsAction extends Action
     public function __invoke(Request $request, Response $response, $args): Response
     {
         if (!isset($request->getQueryParams()['id'])) {
-            throw new Exception("L'id de la categorie n'est pas renseigné");
+            throw new CategoryIdNotProvidenException("L'id de la categorie n'est pas renseigné");
         }
 
         return Twig::fromRequest($request)->render($response, 'prestations.twig',
