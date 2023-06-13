fd9ea0e5a1461c70f9887e2c7dfef1aa297a48b1
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-06-13 08:08:24 +0200

    refactor(api/routes): implement api prefix

--- gift.api/src/conf/routes.php
+++ gift.api/src/conf/routes.php
@@ -9,18 +9,19 @@ use gift\api\actions\FetchAllPrestationsAction;
 use gift\api\actions\FetchAllPrestationsFromCategoryIdAction;
 use gift\api\actions\FetchBoxByIdAction;
 
+const API_PREFIX = '/api';
 
 return function ($app) {
-    $app->get('/api[/]', function ($request, $response, $args) {
+    $app->get(API_PREFIX . '[/]', function ($request, $response, $args) {
         $response->getBody()->write('Hello world!');
         return $response;
     });
 
-    $app->get('/api/categories', FetchAllCategoriesAction::class);
+    $app->get(API_PREFIX . '/categories', FetchAllCategoriesAction::class);
 
-    $app->get('/api/categories/{id}/prestations', FetchAllPrestationsFromCategoryIdAction::class);
+    $app->get(API_PREFIX . '/categories/{id}/prestations', FetchAllPrestationsFromCategoryIdAction::class);
 
-    $app->get('/api/coffrets/{id}', FetchBoxByIdAction::class);
+    $app->get(API_PREFIX . '/coffrets/{id}', FetchBoxByIdAction::class);
 
-    $app->get('/api/prestations', FetchAllPrestationsAction::class);
+    $app->get(API_PREFIX . '/prestations', FetchAllPrestationsAction::class);
 };
